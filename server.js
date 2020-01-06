8341bb9d9df5f156ee47b33835e416a30384b36c
Emmanuel Correa <emmanuel.correa@espol.cl>
2020-01-06 12:17:41 -0300

    configuraciones antes de heroku

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const express = require('express')
 const app = express()
+
+const hbs = require('hbs');
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3000
+
+
 app.use(express.static(__dirname + '/public'))
 
-// app.get('/', function(req, res) {
-// res.send('Hello World')
-// let salida = {
-// nombre: 'Emmanuel',
-// edad: 25,
-// url: req.url
-// }
-// res.send(salida)
-// 
-// })
+//Express HBS
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'EMManuel'
+
+    })
+
+})
+app.get('/about', function(req, res) {
+
+    res.render('about', {
+
+    })
+
+})
 
 app.get('/data', function(req, res) {
     res.send('Hello World')
 
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Escuchando peticiones en el puerto 3000');
 })
